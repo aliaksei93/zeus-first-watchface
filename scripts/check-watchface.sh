7981ee465cf140fc06cf8b559c54bfc193f785fb
Aliaksei Kazlou <lexa.kozllov@gmail.com>
2026-09-22 11:01:06 +0300

    feat: add DSEG7Classic-BoldItalic.ttf

--- scripts/check-watchface.sh
+++ scripts/check-watchface.sh
@@ -96,6 +96,7 @@ done
 for asset in \
   fonts/DSEG-LICENSE.txt \
   fonts/DSEG7Classic-Bold.ttf \
+  fonts/DSEG7Classic-BoldItalic.ttf \
   fonts/INTER-LICENSE.txt \
   fonts/Inter-Bold.ttf \
   fonts/Inter-Regular.ttf \
